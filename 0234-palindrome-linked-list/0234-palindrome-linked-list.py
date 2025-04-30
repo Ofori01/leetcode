0f34873703b4b3e28354c88149a0659d3613a91b
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-04-30 15:06:00 +0000

    Time: 24 ms (80.52%), Space: 39.8 MB (8.76%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # reversing and checking would alter the original head 
+        # using stacks
+        cur = head
+        stack = []
+
+        while cur:
+            stack.append(cur.val)
+            cur = cur.next
+
+        # restart 
+        cur = head
+        while cur:
+            if cur.val != stack.pop():
+                return False
+            cur = cur.next
+        return True
+      
+
+        
