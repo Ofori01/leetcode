dce53d343c71c912017bd9cc00805d69e049efe4
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-05-09 22:37:34 +0000

    Time: 69 ms (85.75%), Space: 23 MB (82.97%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,17 +1,24 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        cur,l,m,res = 0,0,0,0
+        # sliding window way
+        # move right and keep adding odds. 
+        # if number of odds exceed. while, move a mid pointer till the count is up to the k
+        # if the count == k: the ans + m-l + 1. baisically numbers between m and l. Because all those could have also formed a valid subarray
 
+        l,r,m,count = 0,0,0,0
+        res= 0
 
         for r in range(len(nums)):
-            cur += 1 if nums[r]&1 else 0
-            while cur > k:
-                cur-= 1 if nums[l]&1 else 0
+            count += nums[r] & 1
+            # if count exceeds
+            while count > k:
+                count -= nums[l] & 1
                 l+=1
                 m = l
-            if cur == k:
-                while not nums[m] &1:
+            # if count reaches k
+            if count == k:
+                # move mid till count is no longer equal
+                while not nums[m] & 1:
                     m+=1
-                res += m-l+1
+                res+= m-l+1
         return res
-        
