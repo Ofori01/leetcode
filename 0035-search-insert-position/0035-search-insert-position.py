21a2a3c3d940d73912ed512b9ac3cf0d1352029a
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-05-15 14:43:00 +0000

    Time: 0 ms (100%), Space: 18.4 MB (43.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,16 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # or bisect left 
-        # basically first number greater than or equal 
-
+        # binary search for target or first 
+        # were looking for first >=
         l,r = 0,len(nums)
 
         while l < r:
-            mid = (l + r)//2
+
+            mid = (l+r) // 2
 
             if nums[mid] >= target:
                 r = mid
             else:
                 l = mid + 1
         return l
-
