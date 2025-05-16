20dc7f4e6f06e0f52a65f85a7f60398040a5ca3d
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-05-16 17:09:48 +0000

    Time: 4324 ms (5.04%), Space: 23.7 MB (38.02%) - LeetHub

---
+++ 0947-online-election/0947-online-election.py
@@ -0,0 +1,48 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        # actually about finding clossest time to use since times called may not be in the times list
+        # create some form of list that at each time i list[i] tells who is leading
+        self.times = times
+        
+        leaders= defaultdict(int)
+        votes= defaultdict(int)
+        cur_leader = float('-inf')
+        max_votes = float('-inf')
+
+        for i,time in enumerate(times):
+
+            votes[persons[i]]+=1
+
+            if votes[persons[i]] >= max_votes:
+
+                cur_leader = persons[i]
+                max_votes = votes[persons[i]]
+
+            leaders[time] = cur_leader
+
+        self.leaders = leaders
+        print(leaders)
+
+
+    def q(self, t: int) -> int:
+        leaders = self.leaders
+        # binary search time either the exact time or the time just less than t in our leaders. So basically last false
+        times = list(leaders.keys())
+        l, r = 0, len(times)
+
+        while l < r :
+
+            mid = (l+r) // 2
+
+            if times[mid] <= t:
+                l = mid + 1
+            else:
+                r = mid
+
+        return leaders[times[l-1]]
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
