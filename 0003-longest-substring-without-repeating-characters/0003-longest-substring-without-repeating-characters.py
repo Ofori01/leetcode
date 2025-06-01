d16c969c8a328c7672c1792cfaccd829f8cbaf58
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-06-01 23:50:37 +0000

    Time: 20 ms (40.24%), Space: 17.7 MB (96.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,23 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # using last seen index would be cool
-        # for every ch store it's seen index
-        # if ch is seen again move l to last seen + 1
-        # last seen did not work...unto legacy
-
-        # l =0
-        # res =0
-        # last_seen = {}
-
-        # for r in range(len(s)):
-        #     if s[r] in last_seen:
-        #         while l <= last_seen[s[r]]:
-        #             last_seen.pop(s[l])
-        #             l+=1
-        #     last_seen[s[r]] = r
-        #     print(last_seen,l)
-        #     res = max(res, r-l + 1)
-        # return res
         l = 0
         res= 0
         check = {}
