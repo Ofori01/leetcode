80108f9ee8f76b00198a0046c89b7fad037fc580
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-06-02 23:42:42 +0000

    Time: 0 ms (100%), Space: 17.7 MB (98.51%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,6 +1,5 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # binary seach but use the concept of checking the nums mid as a peak if it is not take right sub array and do the same. could also take the left subarray and check same condition holds
 
         l,r = 0 , len(nums)-1
 
