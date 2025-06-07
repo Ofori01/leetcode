0f194ff6e6cba636b29c5229a6acc092051135a2
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-06-07 19:21:18 +0000

    Time: 91 ms (89.31%), Space: 30.8 MB (81.29%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -3,22 +3,19 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
         n = len(matrix)
         m = len(matrix[0])
-        killers = [[0]*(m +1) for _ in range(n+1)]
         # with padding
-        # start from 1,1, every original postion +1
+        killers = [[0]*(m +1) for _ in range(n+1)]
+        
 
         for i in range(n):
             for j in range(m):
                 killers[i+1][j+1]=  matrix[i][j] + killers[i+1][j] + killers[i][j+1] - killers[i][j]
-        print(killers)
         self.killers = killers
 
 
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         killers = self.killers
-        # for sum row1,col1 is upper left and row2,col2 is lower right
-        # so row1,col1 offset by + 1 , go to (row1-1,col2) (row2,col1-1) and (row1-1,col1-1)
         return killers[row2+1][col2+1] -  killers[row1][col2+1] - killers[row2+1][col1] + killers[row1][col1]
 
 
