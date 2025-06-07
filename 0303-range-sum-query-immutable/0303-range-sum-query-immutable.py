78b401d8658bde227312302810de0b2dce2eb635
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-06-07 19:20:27 +0000

    Time: 3 ms (74.36%), Space: 21.7 MB (75.78%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,16 +1,14 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        # normal prefix sum
         prefix = []
         cur = 0
         for i in nums:
             cur+=i
             prefix.append(cur)
         self.prefix = prefix
-
-
         
-
     def sumRange(self, left: int, right: int) -> int:
         if left == 0:
             return self.prefix[right]
