7ea757dfe34393ec79c65c02d964ceedb20741a0
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-06-08 23:17:47 +0000

    Time: 4 ms (42.27%), Space: 17.8 MB (86.82%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # move r till valid, at next ch and move again. if all are valid. return true else return false if something is not valid
-
-        # check = set(wordDict)
-        # res = set()
-        # cur_word = ''
-        # for r in range(len(s)):
-        #     cur_word+=s[r]
-        #     print(cur_word)
-        #     if cur_word in check:
-        #         res.add(cur_word)
-        #         cur_word = ''
-        # if cur_word != '':
-        #     res.add(cur_word)
-        # print(res)
-        # return all(word in check for word in res)
+        # DP PROBLEM
 
         dp = [False]*(len(s)+1)
         dp[len(s)] = True
