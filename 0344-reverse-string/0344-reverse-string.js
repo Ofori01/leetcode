1791ada8f66ed90f00c25f77812d07f6c98056b2
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-09-28 09:05:05 +0000

    Time: 0 ms (100%), Space: 61.4 MB (34.94%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,11 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left = 0, right = s.length -1
-
-    while (left < right) {
-        [s[left], s[right]] = [s[right],s[left]]
-        left ++;
-        right--
-    }
+    s.reverse()
 };
