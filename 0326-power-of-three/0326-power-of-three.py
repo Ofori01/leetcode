eed4e01dd6a66d48ad6324147b0253b958f2b196
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-10-07 15:55:42 +0100

    Time: 23 ms (13.63%), Space: 17.7 MB (93.01%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,13 +1,12 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        # Alla recursion
 
-        # base case
-        # if n == 1, true
-        # if n < 1 , false
-
-        if n == 1:
+        if n == 1 :
             return True
         elif n < 1:
             return False
-        return self.isPowerOfThree(n/3) 
+
+        n /= 3
+
+        return self.isPowerOfThree(n)
+        
