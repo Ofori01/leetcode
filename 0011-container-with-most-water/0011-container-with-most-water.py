ea883ed41f28db9c4721057b2332b8bfea53b6d6
Amoah Ofori Darkwah <128515949+Ofori01@users.noreply.github.com>
2025-10-10 14:09:19 +0100

    Time: 99 ms (67.61%), Space: 28.5 MB (24.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,15 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # find the largest volumn in a greedy way
-        l = 0
-        r = len(height) - 1
-        max_vol = 0
+        res = 0
+
+        l,r = 0, len(height)-1
+
         while l < r:
-            max_vol = max(min(height[l], height[r]) * (r - l), max_vol)
-            if height[l] < height[r]:
-                l+=1
-            else:
+            res = max(res, min(height[l], height[r]) *(r-l))
+            
+
+            if height[l] >= height[r]:
                 r-=1
-        return max_vol
+            else:
+                l+=1
+        return res
 
+            
+            
 
+            
+
+        
